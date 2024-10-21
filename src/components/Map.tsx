3e72c279fcddbb44846012b3e413be79a7242841
Katrina Huang <137211211+katrinahuang888@users.noreply.github.com>
2024-10-21 03:44:33 -0700

    Add files via upload

---
+++ src/components/Map.tsx
@@ -0,0 +1,40 @@
+import "leaflet/dist/leaflet.css";
+import type { Place } from "../api/Place";
+import type { Map as LeafletMap } from "leaflet";
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+// leaflet is a stand-alone library that will be used to display a map in the browser, assuming you are not using React
+// useEffect for panning map around
+// useRef for direct reference to that map
+// react-leaflet is a compatibility layer between the leaflet library and our app
+// MapContainer: coordinates map together
+// TileLayer: map images
+// Marker: dropper icon
+
+interface MapProps {
+  place: Place | null;
+}
+
+export default function Map({ place }: MapProps) {
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[40.7, -74]}
+      zoom={12}
+      scrollWheelZoom
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && <Marker position={[place.latitude, place.longitude]} />}
+    </MapContainer>
+  );
+}
